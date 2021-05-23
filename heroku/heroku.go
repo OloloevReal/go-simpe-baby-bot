5278bc021d92a5e7baa87ad11a213f2dbba6817f
OloloevReal <nickey.n@gmail.com>
2021-05-23 18:40:16 +0300

    changed heroku alive timer

--- heroku/heroku.go
+++ heroku/heroku.go
@@ -30,7 +30,7 @@ func (app *AppHeroku) runHerokuAlive(ctx context.Context) {
 	go func() {
 		defer log.Printf("Closed Heroku alive func, %s", app.ServiceURL)
 		for {
-			t = time.NewTicker(10 * time.Minute)
+			t = time.NewTicker(5 * time.Minute)
 			select {
 			case <-t.C:
 				{
@@ -49,7 +49,7 @@ func (app *AppHeroku) runHerokuAlive(ctx context.Context) {
 func (app *AppHeroku) runHerokuHandler(ctx context.Context) {
 	log.Println("Starting Heroku http handler")
 	fnHandler := func(resp http.ResponseWriter, _ *http.Request) {
-		resp.Write([]byte("Hi there! I'm go-reposter!"))
+		resp.Write([]byte("Hi there! I'm go-simple-baby-bot!"))
 	}
 
 	mux := http.NewServeMux()
